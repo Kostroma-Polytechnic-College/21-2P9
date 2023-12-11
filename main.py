867ce6eacec88418f60c40a8468fd70943d14efa
YuriSilenok <yuri.silenok@gmail.com>
2023-12-11 13:42:59 +0300

    эхо

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from secret import TELEGRAM_TOKEN as token
 import asyncio
 from aiogram import Bot, Dispatcher, executor
+from aiogram.types import Message
 
 bot = Bot(token=token)
 loop = asyncio.get_event_loop()
 dp = Dispatcher(bot, loop=loop)
+
+@dp.message_handler()
+async def messages(message: Message):
+    await bot.send_message(chat_id=message.from_id, text=message.text)
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher=dp)
