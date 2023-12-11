e8629b89337684951fcc862115e97052f102774a
YuriSilenok <yuri.silenok@gmail.com>
2023-12-11 14:25:47 +0300

    эхо

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from secret import TELEGRAM_TOKEN as token
 import asyncio
 from aiogram import Bot, Dispatcher, executor
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 bot = Bot(token=token)
 loop = asyncio.get_event_loop()
@@ -9,7 +9,17 @@ dp = Dispatcher(bot, loop=loop)
 
 @dp.message_handler()
 async def messages(message: Message):
-    await bot.send_message(chat_id=message.from_id, text=message.text)
+    await bot.send_message(
+        chat_id=message.from_id, 
+        text=str(message.from_user),
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[[
+                InlineKeyboardButton(
+                    text=message.text, 
+                    callback_data='1'
+                )]]
+        )
+    )
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher=dp)
