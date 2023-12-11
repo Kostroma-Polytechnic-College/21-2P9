fa4fc7faffaddd8e2965232b12837e11f22217e3
YuriSilenok <yuri.silenok@gmail.com>
2023-12-11 15:20:04 +0300

    Переход по статусам

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ bot = Bot(token=token)
 loop = asyncio.get_event_loop()
 dp = Dispatcher(bot, loop=loop)
 
+@dp.message_handler(commands=["start"])
+async def command_start_handler(message: Message):
+    await bot.send_message(
+        chat_id=message.chat.id, 
+        text="Главное меню",
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[[
+                InlineKeyboardButton(
+                    text="Добавить дисциплину", 
+                    callback_data="11")],[
+                InlineKeyboardButton(
+                    text="На проверку", 
+                    callback_data="12")]]))
+
 @dp.message_handler()
 async def messages(message: Message):
     await bot.send_message(
@@ -23,9 +37,36 @@ async def messages(message: Message):
 
 @dp.callback_query_handler(lambda callback_query: callback_query.data)
 async def callback_query(callback_query: CallbackQuery):
-    await bot.send_message(
-        chat_id=callback_query.message.chat.id, 
-        text=callback_query.data)
+    if callback_query.data == '1':
+        await bot.send_message(
+            chat_id=callback_query.message.chat.id, 
+            text="Главное меню",
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[[
+                    InlineKeyboardButton(
+                        text="Добавить дисциплину", 
+                        callback_data="11")],[
+                    InlineKeyboardButton(
+                        text="На проверку", 
+                        callback_data="12")]]))
+    elif callback_query.data == '11':
+        await bot.send_message(
+            chat_id=callback_query.message.chat.id, 
+            text="Введите название дисциплины",
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[[
+                    InlineKeyboardButton(
+                        text="⬅", 
+                        callback_data="1")]]))
+    elif callback_query.data == '12':
+        await bot.send_message(
+            chat_id=callback_query.message.chat.id, 
+            text="Отправьте файл",
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[[
+                    InlineKeyboardButton(
+                        text="⬅", 
+                        callback_data="1")]]))
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher=dp)
