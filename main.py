9b3e41c1fc004d8dafebb120926109219007f899
YuriSilenok <yuri.silenok@gmail.com>
2023-12-11 14:54:01 +0300

    Обработка клика по кнопке

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from secret import TELEGRAM_TOKEN as token
 import asyncio
 from aiogram import Bot, Dispatcher, executor
-from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 bot = Bot(token=token)
 loop = asyncio.get_event_loop()
@@ -10,16 +10,22 @@ dp = Dispatcher(bot, loop=loop)
 @dp.message_handler()
 async def messages(message: Message):
     await bot.send_message(
-        chat_id=message.from_id, 
+        chat_id=message.chat.id, 
         text=str(message.from_user),
         reply_markup=InlineKeyboardMarkup(
             inline_keyboard=[[
                 InlineKeyboardButton(
                     text=message.text, 
-                    callback_data='1'
+                    callback_data=message.from_user.username
                 )]]
         )
     )
+
+@dp.callback_query_handler(lambda callback_query: callback_query.data)
+async def callback_query(callback_query: CallbackQuery):
+    await bot.send_message(
+        chat_id=callback_query.message.chat.id, 
+        text=callback_query.data)
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher=dp)
